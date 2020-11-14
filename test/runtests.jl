4e8d33234478c49a72239bea6151a63ea328887a
Matt Karikomi <mattkarikomi@gmail.com>
2020-11-13 19:26:57 -0800

    wip

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ checkfn = joinpath(dn,"check.csv")
 @testset "TenX.jl" begin
 	gdf = TenX.loadFolder(gzd)
 
-	check = CSV.read(checkfn)
+	check = CSV.read(checkfn,DataFrame)
     @test Matrix(check) == Matrix(gdf[:,2:end])
 
 	tdf = TenX.loadFolder(tsd)
